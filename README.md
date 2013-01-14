9425166c3e320f6affad87d9ed6ab5ff1260fb4b
Mark Woodall <mawoodall@gmail.com>
2013-01-14 17:46:06 -0600

    Initial working commit

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 trelloLabelize
 ==============
 
-Chrome extension to adjust and format the display of labels
+Chrome extension to add text to the display of labels on cards.  Infrequent users of a board may not remember what the
+label colors stand for.  The extension extends the height of the label on the card and adds the first three letters of the
+name to the box.
+
+
+Install Development Version
+---------------------------
+1. Download the repository as a zip file
+2. Extract zip
+3. Go to Chrome Exensions: [chrome://chrome/extensions/](chrome://chrome/extensions/)
+4. Click on Developer Mode checkbox
+5. Click on Load unpacked extension...
+6. Select the folder containing the source files
+
+Usage
+-----
+This extension will take the first 3 characters of the label name.  If the first three letters are not unique or clear,
+you may want to use a naming convention that puts 3 meaningful letters at the front of the label name.  For example:
+HRR-Human Resources Request.
---
+++ trelloLabelize.js
@@ -0,0 +1,106 @@
+/*!
+ * TrelloLabelize
+ * https://github.com/llad/trelloLabelize
+ * 
+ * Adds text identifiers to labels on Trello cards.
+ *
+ */
+
+// JSLint Options
+/*global $: false, MutationSummary: false */
+/*jslint browser: true, devel: true */
+
+(function () {
+    "use strict";
+
+    // Variables
+    var labelAbbrs = {},
+        labelData,
+        labelSelector = "div.card-labels > div.card-label",
+        cardSelector = "div.card-labels",
+        boardHeaderSelector = "div#board-header",
+        labelEditSelector = "form.edit-labels",
+        labelEditSaveSelector = "form.edit-labels > .js-save",
+        labelHeight = "16px",
+        apiRoot = "https://trello.com/1/boards/";
+        
+    // Makes an API call to get all the names of the labels and takes
+    // a callback so that other processing of the page waits until we get
+    // the list back.
+    function getLabelNames(callback) {
+        var URL = $(location).attr('href'),
+            boardID = URL.split('/')[5],
+            apiURL = apiRoot + boardID;
+        
+        labelAbbrs = {}; //Clear the object
+        
+        // Make the API call
+        $.getJSON(apiURL + "/labelNames", function (data) {
+            $.each(data, function (key, value) {
+                var abbr = value.substr(0, 3),
+                    k = key + "-label";
+                    
+                labelAbbrs[k] = abbr;
+            });
+            
+            callback();  // Makes sure 
+        });
+
+    }
+    
+    // Make the label adjustments cards
+    function labelize() {
+        $(labelSelector).each(function () {
+            var label = $(this).attr('class').split(' ')[1],
+                a = labelAbbrs[label];
+
+            if (a) {
+                $(this).text(a).css({"height": labelHeight});
+            }
+        });
+    }
+    
+    // Gets the list of label names and adjust the card labels
+    function refreshLabels(summaries) {
+        getLabelNames(labelize);
+    }
+    
+    // If the user edits labels, update the cards.
+    function nameChange(summaries) {
+        $(labelEditSaveSelector).click(function () {
+            setTimeout(refreshLabels, 1000);
+        });
+    }
+    
+
+    // *************************************
+    $(document).ready(function () {
+        
+        refreshLabels();
+
+        // Use MutationSummary to track changes to labels on cards
+        var cardObserver = new MutationSummary({
+            callback: labelize,
+            queries: [{
+                element: cardSelector
+            }]
+        }),
+            // Use MutationSummary to track board changes
+            boardObserver = new MutationSummary({
+                callback: refreshLabels, //could have used: function () {getLabelNames(labelize); },
+                queries: [{
+                    element: boardHeaderSelector
+                }]
+            }),
+            
+            // Use MutationSummary to see if user access the edit label popup
+            labelEditObserver = new MutationSummary({
+                callback: nameChange, //could have used: function () {getLabelNames(labelize); },
+                queries: [{
+                    element: labelEditSelector
+                }]
+            });
+
+    });
+    
+}());
